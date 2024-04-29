053b739136fbe21e51909cd8e14d0f91f1f491fc
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-29 22:58:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+
+	const counterElement = document.getElementById('counter');
+    
+    const incrementButton = document.getElementById('incrementBtn');
+    
+    incrementButton.addEventListener('click', function() {
+		
+        let currentValue = parseInt(counterElement.textContent);
+        
+        alert("Un-incremented value: " + currentValue);
+        
+        currentValue++;
+        
+        counterElement.textContent = currentValue;
+    });
+});
